22f1d577546dc71a8423c0c77ac14bab5d0e0216
sidewinder0123 <alaaljamesmatthew@gmail.com>
2024-05-30 23:53:52 +0800

    hahatdog

--- crudlesson/views.py
+++ crudlesson/views.py
@@ -10,7 +10,7 @@ def index_gender(request):
     context = {
         'genders': genders
     }
-    
+
     return render(request, 'gender/index.html', context)
 
 def create_gender(request):
@@ -21,3 +21,56 @@ def store_gender(request):
     Gender.objects.create(gender=gender) #INSERT INTO genders(gender) VALUES(gender)
     messages.success(request, 'Genders successfully saved!')
     return redirect('/genders')
+
+def show_gender(request, gender_id):
+    gender = Gender.objects.get(pk=gender_id) # SELECT * FROM genders WHERE gender_id = gender_id
+
+    context = {
+        'gender': gender, 
+    }
+    
+    return render(request, 'gender/show.html', context)
+
+def edit_gender(request, gender_id):
+    gender = Gender.objects.get(pk=gender_id) # SELECT * FROM genders WHERE gender_id = gender_id
+
+    context = {
+        'gender': gender, 
+    }
+    
+    return render(request, 'gender/edit.html', context)
+
+def update_gender(request, gender_id):
+    gender = request.POST.get('gender')
+
+    Gender.objects.filter(pk=gender_id).update(gender=gender) #UPDATE genders SET gender = gender WHERE gender_id = gender_id
+    messages.success(request, 'Gender successfully updated!')
+
+    return redirect('/genders')
+
+def delete_gender(request, gender_id):
+    gender = Gender.objects.get(pk=gender_id) # SELECT * FROM genders WHERE gender_id = gender_id
+
+    context = {
+        'gender': gender, 
+    }
+    
+    return render(request, 'gender/delete.html', context)
+
+def destroy_gender(request, gender_id):
+    Gender.objects.filter(pk=gender_id).delete() # DELETE FROM genders WHERE gender_id = gender_id
+    messages.success(request, 'Gender successfully deleted!')
+    
+    return redirect('/genders')
+
+
+def index_user(request):
+    return render(request, 'user/index.html')
+
+def create_user(request):
+    return render(request, 'user/create.html')
+    
+
+    
+    
+    
